fc6e4adf71e7c97ebf2c270e1e21d742c84894f4
Eric Buckthal <eric@simplecontacts.com>
2020-02-28 23:33:31 -0500

    s3-ify

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const core = require('@actions/core');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-1' });
 
 try {
-  const inputName = core.getInput('name');
+  const inputBucket = core.getInput('bucket');
   const inputPath = core.getInput('path');
+  const inputKey = core.getInput('key');
 
-  console.log(inputName, inputPath);
+  const stream = fs.createReadStream(inputPath);
+
+  s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+  s3.upload({ Bucket: inputBucket, Key: inputKey, Body: stream }, function(
+    err,
+    data,
+  ) {
+    if (err) {
+      throw err;
+    }
+    core.info(`Uploaded to ${data.Location}`);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
